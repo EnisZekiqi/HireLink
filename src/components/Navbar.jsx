36d088a0ed0b0a25019cdb9bf9aec1097f6006b0
Enis Zekiqi <99254753+EnisZekiqi@users.noreply.github.com>
2025-03-15 12:43:24 +0100

    display job listin with mock data in applications page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,26 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
-        <div className="flex justify-between items-center px-3 py-3 bg-[#1a1a1a] text-[#ece9e7]">
+        <div className="flex justify-between items-center px-3 py-3 bg-[#1a1a1a] text-[#fbfbfb]">
             <h1 className="text-2xl font-semibold">Job Application</h1>
             <div className="flex items-center gap-4">
                 <NavLink to="/"
                     className={({ isActive }) =>
-                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#ece9e7] transition-colors"
+                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#fbfbfb] transition-colors"
                     }
                 >
                  Dashboard
                 </NavLink>
                 <NavLink to="/applications"
                    className={({ isActive }) =>
-                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#ece9e7] transition-colors"
+                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#fbfbfb] transition-colors"
                     }
                 >
                  Applications
                 </NavLink>
                 <NavLink to="/settings"
                    className={({ isActive }) =>
-                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#ece9e7] transition-colors"
+                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#fbfbfb] transition-colors"
                     }
                 >
                  Settings
--- src/pages/Applications.jsx
+++ src/pages/Applications.jsx
@@ -1,8 +1,32 @@
 const Applications = () => {
+
+    const mockJobs = [
+        { id: 1, company: "Company A", position: ".NET Developer" ,status:'Active'},
+        { id: 2, company: "Company B", position: "Frontend Developer",status:'Active'},
+        { id: 3, company: "Company C", position: "Backend Developer",status:'Active' },
+        { id: 4, company: "Company C", position: "Chef",status:'Closed' }
+
+  
+    ]
+
+    const jobsColor = mockJobs.status ==='Active' ? 'text-[#1ff11f]' : 'text[#f11f1f]'
+
     return (
         <div>
-        
-    </div> );
+            <h2 className="text-xl font-medium">Applications</h2>
+
+            {mockJobs.map((jobs) => (
+                <li key={jobs.id} className="flex justify-between items-center p-2 bg-[#262626] text-[#fbfbfb]">
+                    <div className="flex items-center gap-6">
+                        <p>{jobs.company} </p>|<p>Position: {jobs.position}</p>
+                        <span style={{color:jobs.status === 'Active'? '#1ff11f':'#f11f1f'}}>
+                            {jobs.status}
+                        </span>
+                    </div>
+                </li>
+            ))}
+        </div>
+    );
 }
  
 export default Applications;
