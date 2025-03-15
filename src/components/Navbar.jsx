2e86d0a8ec055b8379d0bd7dbc587cd5511b6d83
Enis Zekiqi <99254753+EnisZekiqi@users.noreply.github.com>
2025-03-15 12:15:36 +0100

    added styling on navbar and added active link highlight

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,28 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
-        <div className="flex justify-between px-2 py-1 text-[#000]">
+        <div className="flex justify-between items-center px-3 py-3 bg-[#1a1a1a] text-[#ece9e7]">
             <h1 className="text-2xl font-semibold">Job Application</h1>
-            <div className="flex items-center gap-2">
-                <NavLink to="/">
+            <div className="flex items-center gap-4">
+                <NavLink to="/"
+                    className={({ isActive }) =>
+                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#ece9e7] transition-colors"
+                    }
+                >
                  Dashboard
                 </NavLink>
-                  <NavLink to="/applications">
+                <NavLink to="/applications"
+                   className={({ isActive }) =>
+                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#ece9e7] transition-colors"
+                    }
+                >
                  Applications
                 </NavLink>
-                  <NavLink to="/settings">
+                <NavLink to="/settings"
+                   className={({ isActive }) =>
+                   isActive ? "text-[#4fb0f4] p-1 rounded-md bg-[#262626] transition-colors":"text-[#ece9e7] transition-colors"
+                    }
+                >
                  Settings
                 </NavLink>
             </div>
